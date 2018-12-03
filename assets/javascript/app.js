1d037930d7b4902cb1c86577edeeb8de19f5d4f7
Andrew Dawson <dawso.andrew@gmail.com>
2018-12-03 17:38:37 -0500

    Adds a results table to the quiz.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,6 +10,7 @@ class Question {
 
 
 let intervalId = null;
+let results;
 let slideNumber;
 const secondsPerQuestion = 1;
 const questions = [
@@ -17,6 +18,58 @@ const questions = [
   new Question("What is the second question?", ["If", "I", "Had", "No", "Limbs"], 3),
 ];
 
+
+function chooseOption(questionIndex, answer) {
+  results[questionIndex] = answer;
+  clearInterval(intervalId);
+  resetInterval();
+}
+
+function createResultsTable() {
+  let table = $("<table>");
+
+  let headerRow = $("<tr>");
+  headerRow.append("<th>Number</th>");
+  headerRow.append("<th>Choice</th>");
+  headerRow.append("<th>Answer</th>");
+  table.append(headerRow);
+  
+  for (let i = 0; i < questions.length; i++) {
+    const question = questions[i];
+
+    const questionNumber = i + 1;
+
+    const choiceIndex = results[i];
+    let choice = "Unanswered";
+    if (choiceIndex !== undefined) {
+      choice = question.options[choiceIndex];
+    }
+
+    const answer = question.options[question.answer];
+
+    let row = $("<tr>");
+    row.append("<td>" + questionNumber +"</td>");
+    row.append("<td>" + choice +"</td>");
+    row.append("<td>" + answer +"</td>");
+    table.append(row);
+  }
+
+  return table;
+}
+
+function endQuiz() {
+  clearInterval(intervalId);
+
+  $("#game-panel").empty();
+  $("#game-panel").append("<h2>Quiz Finished!</h2>");
+
+  const table = createResultsTable();
+  $("#game-panel").append(table);
+
+  let retryButton = $("<button type=\"button\">Retry</button>");
+  retryButton.one("click", startQuiz);
+  $("#game-panel").append(retryButton);
+}
 
 function nextSlide() {
   if (slideNumber < questions.length) {
@@ -27,38 +80,42 @@ function nextSlide() {
   slideNumber++;
 }
 
-function showQuestion(questionIndex) {
-  let next = questions[questionIndex];
-  $("#game-panel").empty();
-
-  let title = $("<h2>" + next.question + "</h2>");
-  $("#game-panel").append(title);
-
-  let form = $("<form>");
-  for (let i = 0; i < next.options.length; i++) {
-    let option = next.options[i];
-    let button = $("<button>");
-    button.attr("value", i);
-    button.text(option);
-    form.append(button);
-  }
-  $("#game-panel").append(form);
-}
-
-function endQuiz() {
-  $("#game-panel").html("<h2>Quiz Finished!</h2>");
-  clearInterval(intervalId);
-
-  let retryButton = $("#game-panel").append("<button>Retry</button>");
-  retryButton.click(startQuiz);
-}
-
-function startQuiz() {
-  slideNumber = 0;
-  intervalId = setInterval(nextSlide, 1000 * secondsPerQuestion);
+function resetInterval() {
+  intervalId = setInterval(nextSlide, 5000 * secondsPerQuestion);
   nextSlide();
 }
 
+function showQuestion(questionIndex) {
+  const next = questions[questionIndex];
+  $("#game-panel").empty();
+
+  const title = $("<h2>" + next.question + "</h2>");
+  $("#game-panel").append(title);
+
+  let form = $("<form>");
+
+  for (let i = 0; i < next.options.length; i++) {
+    const option = next.options[i];
+    let button = $("<button type=\"button\">");
+    button.attr("value", i);
+    button.text(option);
+    button.one("click", (event) => {
+      const value = $(event.currentTarget).val();
+      const answer = parseInt(value);
+      chooseOption(questionIndex, answer);
+    });
+    form.append(button);
+  }
+
+  $("#game-panel").append(form);
+}
+
+function startQuiz() {
+  results = [];
+  slideNumber = 0;
+  resetInterval();
+}
+
 $(document).ready(() => {
-  $("#start-button").click(startQuiz);
+  $("#start-button").one("click", startQuiz);
 });

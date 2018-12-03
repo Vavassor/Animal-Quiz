714c81ca0238f70787be8ac1cf6877aefa1e10bc
Andrew Dawson <dawso.andrew@gmail.com>
2018-12-03 16:17:43 -0500

    Shows several questions in order with a retry button at the end.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,64 @@
 "use strict";
 
-const firstQuestion = "<h2>What is the first question?</h2>" +
-  "<form>" +
-    "<input id=\"answer-0\" name=\"answers\" type=\"radio\" value=\"0\">"
-    "<label for=\"answer-0\">A</label>" +
-    "<input id=\"answer-1\" name=\"answers\" type=\"radio\" value=\"1\">" +
-    "<label for=\"answer-1\">B</label>" +
-    "<input id=\"answer-2\" name=\"answers\" type=\"radio\" value=\"2\">" +
-    "<label for=\"answer-2\">C</label>" +
-  "</form>";
+class Question {
+  constructor(question, options, answer) {
+    this.question = question;
+    this.options = options;
+    this.answer = answer;
+  }
+}
 
-function startGame() {
-  $("#game-panel").html(firstQuestion);
+
+let intervalId = null;
+let slideNumber;
+const secondsPerQuestion = 1;
+const questions = [
+  new Question("What is the first question?", ["Nothing", "Something Else", "Fancy"], 2),
+  new Question("What is the second question?", ["If", "I", "Had", "No", "Limbs"], 3),
+];
+
+
+function nextSlide() {
+  if (slideNumber < questions.length) {
+    showQuestion(slideNumber);
+  } else {
+    endQuiz();
+  }
+  slideNumber++;
+}
+
+function showQuestion(questionIndex) {
+  let next = questions[questionIndex];
+  $("#game-panel").empty();
+
+  let title = $("<h2>" + next.question + "</h2>");
+  $("#game-panel").append(title);
+
+  let form = $("<form>");
+  for (let i = 0; i < next.options.length; i++) {
+    let option = next.options[i];
+    let button = $("<button>");
+    button.attr("value", i);
+    button.text(option);
+    form.append(button);
+  }
+  $("#game-panel").append(form);
+}
+
+function endQuiz() {
+  $("#game-panel").html("<h2>Quiz Finished!</h2>");
+  clearInterval(intervalId);
+
+  let retryButton = $("#game-panel").append("<button>Retry</button>");
+  retryButton.click(startQuiz);
+}
+
+function startQuiz() {
+  slideNumber = 0;
+  intervalId = setInterval(nextSlide, 1000 * secondsPerQuestion);
+  nextSlide();
 }
 
 $(document).ready(() => {
-  $("#start-button").click(() => {
-    startGame();
-  });
+  $("#start-button").click(startQuiz);
 });
